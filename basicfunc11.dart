f5a2fd5175dbd3d99cf148c8e65b0d794c732d17
olimjonsn233 <olimjonsn233@gmail.com>
2022-05-21 16:34:14 +0500

    create dart file

---
+++ basicfunc11.dart
@@ -0,0 +1,10 @@
+// Create a function called func.
+// Create function argument a.
+// Return the absolute value of a.
+void main() {
+  print(func(5));
+}
+
+func(int a) {
+  return a.abs();
+}
---
+++ basicfunc17.dart
@@ -0,0 +1,10 @@
+//  Create a function called main.
+
+//  Return the value null
+void main() {
+  print(func());
+}
+
+func() {
+  return;
+}
