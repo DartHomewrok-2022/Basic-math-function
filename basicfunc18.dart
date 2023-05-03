fa05607f629601591c15a77ebe36aae44505d78e
Sanjarbek Saidov <71938677+Sanjarbek17@users.noreply.github.com>
2023-05-03 15:34:24 +0500

    Update basicfunc18.dart

--- basicfunc18.dart
+++ basicfunc18.dart
@@ -3,5 +3,5 @@ import 'dart:math';
 // Create a function called main.
 // Create function arguments a.
 // Assign the value pi to the variable "a" and return.
-// Round the result to 2 decimal places.
+// Round the result.
 void main() {}
